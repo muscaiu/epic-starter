2e691475ec9548b1179ed1f2677e1cf712df6939
Cristian Muscalu <muscaiu@yahoo.com>
2023-11-18 15:11:22 +0200

    separated Document and Nav

---
+++ app/components/root/Nav.tsx
@@ -0,0 +1,39 @@
+import { Link, useMatches } from '@remix-run/react'
+import React from 'react'
+import { useOptionalUser } from '#app/utils/user.ts'
+import UserDropdown from '../nav/UserDropdown.tsx'
+import { SearchBar } from '../search-bar.tsx'
+import { Button } from '../ui/button.tsx'
+import ThemeSwitch from './ThemeSwitch.tsx'
+
+export default function Nav({ data }: { data: any }) {
+  const user = useOptionalUser()
+  const matches = useMatches()
+
+  const isOnSearchPage = matches.find(m => m.id === 'routes/users+/index')
+  const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
+
+  return (
+    <nav>
+      <div className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
+        <Link to="/">
+          <div className="font-light">Home</div>
+        </Link>
+        <div className="ml-auto hidden max-w-sm flex-1 sm:block">
+          {searchBar}
+        </div>
+        <div className="flex items-center gap-10">
+          {user ? (
+            <UserDropdown />
+          ) : (
+            <Button asChild variant="default" size="sm">
+              <Link to="/login">Log In</Link>
+            </Button>
+          )}
+        </div>
+        <ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
+        <div className="block w-full sm:hidden">{searchBar}</div>
+      </div>
+    </nav>
+  )
+}
--- app/root.tsx
+++ app/root.tsx
@@ -8,33 +8,25 @@ import {
 	type MetaFunction,
 } from '@remix-run/node'
 import {
-	Link,
-	Links,
-	LiveReload,
-	Meta,
 	Outlet,
-	Scripts,
-	ScrollRestoration,
 	useLoaderData,
-	useMatches,
 } from '@remix-run/react'
 import { withSentry } from '@sentry/remix'
 import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
 import { HoneypotProvider } from 'remix-utils/honeypot/react'
 import { z } from 'zod'
 import { Confetti } from './components/confetti.tsx'
-import UserDropdown from './components/nav/UserDropdown.tsx'
 import { EpicProgress } from './components/progress-bar.tsx'
-import ThemeSwitch, { useTheme } from './components/root/ThemeSwitch.tsx'
-import { SearchBar } from './components/search-bar.tsx'
+import Document from './components/root/Document.tsx'
+import Nav from './components/root/Nav.tsx'
+import { useTheme } from './components/root/ThemeSwitch.tsx'
 import { EpicToaster } from './components/toaster.tsx'
-import { Button } from './components/ui/button.tsx'
 
 import { href as iconsHref } from './components/ui/icon.tsx'
 import fontStyleSheetUrl from './styles/font.css'
 import tailwindStyleSheetUrl from './styles/tailwind.css'
 import { getUserId, logout } from './utils/auth.server.ts'
-import { ClientHintCheck, getHints } from './utils/client-hints.tsx'
+import { getHints } from './utils/client-hints.tsx'
 import { getConfetti } from './utils/confetti.server.ts'
 import { csrf } from './utils/csrf.server.ts'
 import { prisma } from './utils/db.server.ts'
@@ -42,10 +34,9 @@ import { getEnv } from './utils/env.server.ts'
 import { honeypot } from './utils/honeypot.server.ts'
 import { combineHeaders, getDomainUrl } from './utils/misc.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
-import { type Theme, setTheme, getTheme } from './utils/theme.server.ts'
+import { setTheme, getTheme } from './utils/theme.server.ts'
 import { makeTimings, time } from './utils/timing.server.ts'
 import { getToast } from './utils/toast.server.ts'
-import { useOptionalUser } from './utils/user.ts'
 
 export const links: LinksFunction = () => {
 	return [
@@ -115,10 +106,9 @@ export async function loader({ request }: DataFunctionArgs) {
 		: null
 	if (userId && !user) {
 		console.info('something weird happened')
-		// something weird happened... The user is authenticated but we can't find
-		// them in the database. Maybe they were deleted? Let's log them out.
 		await logout({ request, redirectTo: '/' })
 	}
+
 	const { toast, headers: toastHeaders } = await getToast(request)
 	const { confettiId, headers: confettiHeaders } = getConfetti(request)
 	const honeyProps = honeypot.getInputProps()
@@ -182,82 +172,20 @@ export async function action({ request }: DataFunctionArgs) {
 	return json({ success: true, submission }, responseInit)
 }
 
-export function Document({
-	children,
-	nonce,
-	theme = 'light',
-	env = {},
-}: {
-	children: React.ReactNode
-	nonce: string
-	theme?: Theme
-	env?: Record<string, string>
-}) {
-	return (
-		<html lang="en" className={`${theme} h-full overflow-x-hidden`}>
-			<head>
-				<ClientHintCheck nonce={nonce} />
-				<Meta />
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width,initial-scale=1" />
-				<Links />
-			</head>
-			<body className="bg-background text-foreground">
-				{children}
-				<script
-					nonce={nonce}
-					dangerouslySetInnerHTML={{
-						__html: `window.ENV = ${JSON.stringify(env)}`,
-					}}
-				/>
-				<ScrollRestoration nonce={nonce} />
-				<Scripts nonce={nonce} />
-				<LiveReload nonce={nonce} />
-			</body>
-		</html>
-	)
-}
-
 function App() {
 	const data = useLoaderData<typeof loader>()
 	const nonce = useNonce()
-	const user = useOptionalUser()
 	const theme = useTheme()
-	const matches = useMatches()
-	const isOnSearchPage = matches.find(m => m.id === 'routes/users+/index')
-	const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
 
 	return (
 		<Document nonce={nonce} theme={theme} env={data.ENV}>
 			<div className="flex h-screen flex-col justify-between">
 				<header className="container py-6">
-					<nav>
-						<div className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-							<Link to="/">
-								<div className="font-light">Home</div>
-							</Link>
-							<div className="ml-auto hidden max-w-sm flex-1 sm:block">
-								{searchBar}
-							</div>
-							<div className="flex items-center gap-10">
-								{user ? (
-									<UserDropdown />
-								) : (
-									<Button asChild variant="default" size="sm">
-										<Link to="/login">Log In</Link>
-									</Button>
-								)}
-							</div>
-							<ThemeSwitch userPreference={data.requestInfo.userPrefs.theme} />
-							<div className="block w-full sm:hidden">{searchBar}</div>
-						</div>
-					</nav>
+					<Nav data={data} />
 				</header>
-
 				<div className="flex-1">
 					<Outlet />
 				</div>
-
 			</div>
 			<Confetti id={data.confettiId} />
 			<EpicToaster toast={data.toast} />
